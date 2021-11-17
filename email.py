6e14150be813705a16cc81b0fb29347986c871e9
Michael Laigaard <laigaard.michael@gmail.com>
2021-11-16 22:37:47 -0700

    set up basic structure based on realpython.com article

---
+++ email.py
@@ -0,0 +1,29 @@
+## Import Packages
+import smtplib, ssl
+
+## Establish email variable
+port = 465
+sender_email = ""
+receiver_email = ""
+password = input("Please enter your password: ")
+#  ^^Use getpass module to make this more secure?
+message = """\
+Subject: Hello World
+
+I sent this email using a Python script.
+"""
+
+
+# Create Secure SSL Context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+
+
+## NEXT STEPS:
+## Expand to multiple emails
+## Include attachments
+## Establish dictionary of lenders --> main and CC emails
+## Send the same email multiple times (same exact email, different recipients) --> Use  a loop?
