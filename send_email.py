b68504fd44b0947a0d72d65093263338bbbcbe16
Michael Laigaard <laigaard.michael@gmail.com>
2021-11-23 22:16:27 -0700

    successfully sent message with two attachments

--- send_email.py
+++ send_email.py
@@ -24,27 +24,28 @@ message["Cc"] = cc_email
 ## Add body to message
 message.attach(MIMEText(body, "plain"))
 
-filename = "testDoc.pdf"
+filename = ["testDoc.pdf", "testDoc2.pdf"]
 
 # Open PDF file in binary mode
-with open(filename, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+for file in filename:
+    with open(file, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-    # Encode file in ASCII characters to send by email
-encoders.encode_base64(part)
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
 
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {file}",
+        )
 
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
 
 # Log in to server using secure context and send email
 context = ssl.create_default_context()
@@ -55,6 +56,5 @@ with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
 
 ## NEXT STEPS:
 ## send to multiple CC emails (can't use list, "'list' object has no attribute 'encode'")
-##include multiple attachments (read up on add_header function)
 ## Establish dictionary of 'lenders' --> main and CC emails
 ## Send the same email multiple times (same exact email, different recipients) --> Use  a loop?
