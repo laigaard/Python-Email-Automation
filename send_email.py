3c0973e4d761072a83faae5dbf4033168b24c8c2
Michael Laigaard <laigaard.michael@gmail.com>
2021-12-28 22:43:35 -0700

    reorganized code so all email building logic is within the function

--- send_email.py
+++ send_email.py
@@ -13,7 +13,6 @@ start = time.time()
 subject = "New Funding Submission from Laigaard Capital"
 body = "Laigaard Capital would like to submit a funding application from a business we feels meets your qualifications, please let me know if you need anything else.\n\nThank you!"
 sender_email = "laigaard.dev@gmail.com"
-receiver_email = "laigaard.dev@gmail.com"
 cc_email = "laigaard.dev+cc@gmail.com, laigaard.dev+cc2@gmail.com"
 password = getpass.getpass()
 filename = ["files/testDoc.pdf", "files/statement1.pdf", "files/statement2.pdf", "files/statement3.pdf"]
@@ -21,7 +20,7 @@ filename = ["files/testDoc.pdf", "files/statement1.pdf", "files/statement2.pdf",
 
 # testing lender data structures, need to update receive/cc emails in script
 abc_cap = {"name": "ABC Capital", "submission_email": "laigaard.dev@gmail.com", "cc_email": ["laigaard.dev+cc@gmail.com"]}
-get_funded = {"name": "123 GetFunded", "submission_email": "laigaard.dev@gmail.com", "cc_email": ["laigaard.dev+cc@gmail.com","laigaard.dev+cc2@gmail.com"]}
+get_funded = {"name": "123 GetFunded", "submission_email": "test.dev7105@gmail.com", "cc_email": ["laigaard.dev+cc@gmail.com","laigaard.dev+cc2@gmail.com"]}
 xyz_vent = {"name": "XYZ Ventures", "submission_email": "laigaard.dev@gmail.com", "cc_email": ["laigaard.dev+cc@gmail.com", "laigaard.dev+cc2@gmail.com", "laigaard.dev+cc3@gmail.com"]}
 
 lenders = [abc_cap, get_funded, xyz_vent]
@@ -31,46 +30,44 @@ lenders = [abc_cap, get_funded, xyz_vent]
 def send_message():
     i = 0
     while i < len(lenders):
-        # receiver_email = lenders[i]["submission_email"]
+        receiver_email = lenders[i]["submission_email"]
         # cc_email = lenders[i]["cc_email"]                 this is a list, strip/split list?  can't use list in email script
+
+        ## Create MIMEMultipart message headers
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Cc"] = cc_email
+
+        ## Add body to message
+        message.attach(MIMEText(body, "plain"))
+
+        # Open PDF file in binary mode
+        for file in filename:
+            with open(file, "rb") as attachment:
+                # Add file as application/octet-stream
+                part = MIMEBase("application", "octet-stream")
+                part.set_payload(attachment.read())
+
+                # Encode file in ASCII characters to send by email
+                encoders.encode_base64(part)
+
+                # Add header as key/value pair to attachment part
+                part.add_header(
+                    "Content-Disposition",
+                    f"attachment; filename= {file}",
+                )
+
+                # Add attachment to message and convert message to string
+                message.attach(part)
+                text = message.as_string()
+
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, text)
         i += 1
-
-        # todo - MIMEMultipart message created in the function, may be easiest to just create whole header in the function
-
-## Create MIMEMultipart message headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
-message["Cc"] = cc_email
-
-## Add body to message
-message.attach(MIMEText(body, "plain"))
-
-# Open PDF file in binary mode
-for file in filename:
-    with open(file, "rb") as attachment:
-        # Add file as application/octet-stream
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-
-        # Encode file in ASCII characters to send by email
-        encoders.encode_base64(part)
-
-        # Add header as key/value pair to attachment part
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename= {file}",
-        )
-
-        # Add attachment to message and convert message to string
-        message.attach(part)
-        text = message.as_string()
-
 
 
 send_message()
@@ -80,6 +77,3 @@ end = time.time()
 result = end - start
 print("Email Sent.")
 print(f"It took {result} seconds to send.")
-
-
-# TODO: Create readme for github
