b41d1faf79fb4b5ccfa263edd8c2193da5030270
Michael Laigaard <laigaard.michael@gmail.com>
2022-01-02 19:59:12 -0700

    subject line now customized to applicant name

--- send_email.py
+++ send_email.py
@@ -10,41 +10,40 @@ from email.mime.text import MIMEText
 start = time.time()
 
 ## Establish email variables that are the same for all emails
-subject = "New Funding Submission from Laigaard Capital"
+applicant_name = "A Small Business Co."
+subject = f"New Funding Submission from Laigaard Capital - {applicant_name}"
 body = "Laigaard Capital would like to submit a funding application from a business we feels meets your qualifications, please let me know if you need anything else.\n\nThank you!"
 sender_email = "laigaard.dev@gmail.com"
 password = getpass.getpass()
 attach_files = ["files/testDoc.pdf", "files/statement1.pdf", "files/statement2.pdf", "files/statement3.pdf"]
 
 ## Establish a dictionary for each lender you work with
-abc_cap = {"name": "ABC Capital", "submission_email": "laigaard.dev@gmail.com", "cc_email": ["test.dev7105@gmail.com"]}
-get_funded = {"name": "123 GetFunded", "submission_email": "test.dev7105@gmail.com", "cc_email": ["laigaard.dev@gmail.com",""]}
-xyz_vent = {"name": "XYZ Ventures", "submission_email": "", "cc_email": ["laigaard.dev@gmail.com", "test.dev7105@gmail.com", ""]}
+abc_cap = {"name": "ABC Capital", "submission_email": "laigaard.dev@gmail.com", "cc_email": ["test.dev7105@gmail.com", "laigaard.dev+cc@gmail.com"]}
+get_funded = {"name": "123 GetFunded", "submission_email": "", "cc_email": [""]}
+xyz_vent = {"name": "XYZ Ventures", "submission_email": "", "cc_email": [""]}
 
 ## Add the lenders to a list, only add lenders you want to send this specific email to
-lenders = [abc_cap, get_funded, xyz_vent]
-
+lenders = [abc_cap]
 
 ## Function to loop through lender list and send an email to each one
 def send_message():
     i = 0
+    receiver_email_final = []
     while i < len(lenders):
         receiver_email = lenders[i]["submission_email"]
         cc_list = lenders[i]["cc_email"]
-        cc_email_str = ''
+        cc_list_str = ""
+        receiver_email_final.append(receiver_email)
         for cc in cc_list:
-            if len(cc_list) == 1:
-                cc_email_str += cc
-            else:
-                cc_email_str += cc + ','
-        print(cc_email_str)
+            cc_list_str += cc + ", "
+            receiver_email_final.append(cc)
 
         ## Create MIMEMultipart message headers
         message = MIMEMultipart()
         message["From"] = sender_email
         message["To"] = receiver_email
+        message["Cc"] = cc_list_str
         message["Subject"] = subject
-        message["Cc"] = cc_email_str
 
         ## Add body to message
         message.attach(MIMEText(body, "plain"))
@@ -62,10 +61,11 @@ def send_message():
                 message.attach(part)
                 text = message.as_string()
 
+        # receiver_email_final =
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, text)
+            server.sendmail(sender_email, receiver_email_final, text)
         i += 1
 
 send_message()
